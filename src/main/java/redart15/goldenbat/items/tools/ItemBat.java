27ebee223d4d2bd665f5b6dea6c1bc6ec849a882
Redart15 <redart15@users.noreply.github.com>
2025-07-27 01:41:01 +0200

    made itembat inherit from sword, simplified some of the logic

--- src/main/java/redart15/goldenbat/items/tools/ItemBat.java
+++ src/main/java/redart15/goldenbat/items/tools/ItemBat.java
@@ -1,20 +1,13 @@
 package redart15.goldenbat.items.tools;
 
 import net.minecraft.core.block.Block;
-import net.minecraft.core.entity.Entity;
-import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.material.ToolMaterial;
+import net.minecraft.core.item.tool.ItemToolSword;
 
-import static redart15.goldenbat.items.tools.Smashables.isSmashable;
-
-public class ItemBat extends Item {
-	private final int weaponDamage;
-
+public class ItemBat extends ItemToolSword {
 	public ItemBat(String name, String namespaceId, int id, ToolMaterial material) {
-		super(name, namespaceId, id);
-		this.maxStackSize = 1;
-		this.weaponDamage = 2 + material.getDamage() * 2;
+		super(name, namespaceId, id, material);
 	}
 
 	@Override
@@ -22,11 +15,6 @@ public class ItemBat extends Item {
 		if(block == null){
 			return 1.5F;
 		}
-		return isSmashable(block.id()) ? 30.0F : 1.5F;
-	}
-
-	@Override
-	public int getDamageVsEntity(Entity entity, ItemStack is) {
-		return this.weaponDamage;
+		return Smashables.instance.isSmashable(block.id()) ? 30.0F : 1.5F;
 	}
 }
--- src/main/java/redart15/goldenbat/items/tools/ItemGoldenBat.java
+++ src/main/java/redart15/goldenbat/items/tools/ItemGoldenBat.java
@@ -1,5 +1,10 @@
 package redart15.goldenbat.items.tools;
+import net.minecraft.core.entity.Entity;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.item.material.ToolMaterial;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
 
 public class ItemGoldenBat extends ItemBat {
 
@@ -7,4 +12,20 @@ public class ItemGoldenBat extends ItemBat {
 		super(name, namespaceId, id, material);
 		this.setMaxDamage(0); // makes the item unbreakable
 	}
+
+	@Override
+	public boolean isSilkTouch() {
+		return false;
+	}
+	@Override
+	public int getDamageVsEntity(Entity entity, ItemStack is){
+		return 1;
+	}
+
+	@Override
+	public ItemStack onUseItem(ItemStack itemstack, World world, Player entityplayer) {
+		entityplayer.yd = entityplayer.xd = entityplayer.zd = 2;
+		return itemstack;
+	}
+
 }
